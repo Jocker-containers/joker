9ce350127706613645243678c76bdb8daaf93107
nazar <nazar.demchvk@gmail.com>
2023-11-19 01:17:51 +0200

    fixed help message (works if input is incorrect)

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 use clap::{arg, Command};
 
-
 pub fn cli() -> Command {
     Command::new("joker")
+        .arg_required_else_help(true)
         .about("A cli component of the joker project.")
         .subcommand_required(true)
         .arg_required_else_help(true)
@@ -27,6 +27,64 @@ pub fn cli() -> Command {
             Command::new("run")
                 .about("Runs specified containers on a current daemon.")
                 .arg_required_else_help(true)
-                .arg(arg!(<CONTAINER_NAME> ... "Stuff to add")),
+                .arg(arg!(<CONTAINER_NAME> ... "Stuff to add"))
+                .arg_required_else_help(true),
         )
+}
+
+pub fn execute(command: &mut Command) {
+    let matches = command.clone().get_matches();
+    match matches.subcommand() {
+        Some(("add", sub_matches)) => {
+            let daemon_name = sub_matches.get_one::<String>("DAEMON_NAME").expect("required");
+            let ip_addr = sub_matches.get_one::<String>("ip").expect("required");
+            let port = sub_matches.get_one::<String>("port").expect("required");
+
+            add_daemon(daemon_name, ip_addr, port);
+        }
+        Some(("checkout", sub_matches)) => {
+            let daemon_name = sub_matches.get_one::<String>("DAEMON_NAME").expect("required");
+
+            checkout_daemon(daemon_name);
+        }
+        Some(("run", sub_matches)) => {
+            let containers = sub_matches
+                .get_many::<String>("CONTAINER_NAME")
+                .into_iter()
+                .flatten()
+                .map(|x| x.as_str())
+                .collect::<Vec<_>>();
+
+            run_container(&containers);
+        }
+        _ => show_help_message(command),
+    }
+}
+
+fn add_daemon(daemon_name: &str, ip_addr: &str, port: &str) {
+    println!(
+        "Creating daemon {} at ip {} and port {}.",
+        daemon_name,
+        ip_addr,
+        port,
+    );
+}
+
+fn checkout_daemon(daemon_name: &str) {
+    println!(
+        "Switching to daemon {}.",
+        daemon_name,
+    );
+}
+
+fn run_container(containers: &[&str]) {
+    println!(
+        "Running containers {} at daemon {}.",
+        containers.join(", "),
+        "current daemon".to_owned(),
+    );
+}
+
+fn show_help_message(command: &mut Command) {
+    command.print_help().unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,6 @@
 use joker::*;
 
 fn main() {
-    let matches = cli().get_matches();
-
-    match matches.subcommand() {
-        Some(("add", sub_matches)) => {
-            println!(
-                "Creating daemon {} at ip {} and port {}.",
-                sub_matches.get_one::<String>("DAEMON_NAME").expect("required"),
-                sub_matches.get_one::<String>("ip").expect("required"),
-                sub_matches.get_one::<String>("port").expect("required"),
-            );
-        }
-        Some(("checkout", sub_matches)) => {
-            println!(
-                "Switching to daemon {}.",
-                sub_matches.get_one::<String>("DAEMON_NAME").expect("required"),
-            );
-        }
-        Some(("run", sub_matches)) => {
-            let args = sub_matches
-                .get_many::<String>("CONTAINER_NAME")
-                .into_iter()
-                .flatten()
-                .map(|x| x.as_str())
-                .collect::<Vec<_>>()
-                .join(", ");
-            println!(
-                "Running containers {} at daemon {}.",
-                args,
-                "current daemon".to_owned(),
-            );
-        }
-        _ => unreachable!(),
-    }
+    let mut command = cli();
+    execute(&mut command);
 }
